d88ad94e9d73743289e98437135673bd07324437
D-lyw <liuyuanwang4321@gmail.com>
2023-03-09 16:15:18 +0800

    chore: update service fee

--- components/LendNFT/InstallmentLendConfig.tsx
+++ components/LendNFT/InstallmentLendConfig.tsx
@@ -467,7 +467,7 @@ const InstallmentLendConfig: React.FC<LendConfigProps> = (props) => {
         </Box>
         <Box className={styles.serviceFee}>
           <Typography>Service fee</Typography>
-          <Typography variant="h5">10%</Typography>
+          <Typography variant="h5">0%</Typography>
         </Box>
 
         {errorMessage &&
